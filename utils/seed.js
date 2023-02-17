f19f7fabfde6d005f44fc240f0b58a7a86dd00a4
Anne <aepanagotopulos@gmail.com>
2023-02-16 22:54:44 -0500

    updates

---
+++ utils/seed.js
@@ -0,0 +1,19 @@
+// const mongoose = require('mongoose');
+// const Thought = require("../models/thought");
+// const User = require("../models/user")
+
+// const seedThought = [
+
+// ];
+
+// const seedUser = [
+
+// ];
+
+// const seedDB = async () => {
+//     await Thought.deleteMany({})
+//     await Thought.insertMany(seedThought)
+//     await User.deleteMany({})
+//     await User.insertMany(seedUser)
+// };
+
